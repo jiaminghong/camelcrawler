a5db134b75cf9e84def77fc54671b56781759b0f
chi1.zhang <chi.zhang@bkjk.com>
2018-12-14 06:53:30 -0500

    api modified

--- CrawlerAPI/app/controllers/GetDataController.scala
+++ CrawlerAPI/app/controllers/GetDataController.scala
@@ -2,10 +2,11 @@ package controllers
 
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
+import akka.util.ByteString
 import play.api.mvc._
 import redis.RedisClient
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -28,26 +29,29 @@ class GetDataController extends Controller {
   implicit val akkaSystem = akka.actor.ActorSystem()
     val redis1 = RedisClient(db = Some(1))
     val backlinks = redis1.lrange(s+"_outbound",0,-1)
-    val listurl : String = ""
+    var listurl :Seq[ByteString] = Seq()
     backlinks onComplete {
-      case Success(urls) => for (url <- urls) yield listurl.concat(url.utf8String)
+      case Success(urls: Seq[ByteString]) => listurl = urls
       case Failure(t) => println("An error has occurred: " + t.getMessage)
     }
-    Ok
+    Thread.sleep(5000)
+    val lists: Seq[String] = for (a <- listurl) yield a.utf8String
+    Ok(lists)
   }
 }
 
 object GetDataController extends App {
   implicit val akkaSystem = akka.actor.ActorSystem()
   val link = "https://www.firesticktricks.com/dmca-policy"
-  var listurl = List("")
+  var listurl :Seq[ByteString] = Seq()
   val redis1 = RedisClient(db = Some(1))
   val key = link+"_outbound"
-  val backlinks = redis1.lrange(link+"_outbound",0,-1)
+  val backlinks: Future[Seq[ByteString]] = redis1.lrange(link+"_outbound",0,-1)
   backlinks onComplete {
-    case Success(urls) => for(url <- urls) yield listurl :: url.utf8String
+    case Success(urls: Seq[ByteString]) => listurl = urls
     case Failure(t) => println("An error has occurred: " + t.getMessage)
   }
-  Thread.sleep(10000)
-  println("listurl" + listurl)
+  Thread.sleep(5000)
+  val lists: Seq[String] = for (a <- listurl) yield a.utf8String
+  println("listurl" + lists)
 }
